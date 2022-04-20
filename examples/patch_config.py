ef7f8385510d0eeb1a74d83aa094419377df6f7d
Nikita Melkozerov <nikita@some.engineering>
2022-04-20 15:00:17 +0200

    psk handling

--- examples/patch_config.py
+++ examples/patch_config.py
@@ -1,12 +1,12 @@
 from time import sleep
 from resotoclient import ResotoClient
-
+import os
 
 # Create a ResotoClient object.
-# in case you have psk available, you can use it here so that
+# in case you configured psk, use it here so that
 # Resoto is able to verify the TLS certificate.
-client = ResotoClient(url="https://localhost:8900", psk=None)
-
+psk = os.environ.get("PSK", "changeme")
+client = ResotoClient(url="https://localhost:8900", psk=psk)
 
 def wait_for_resotocore(client: ResotoClient):
     while True:
--- examples/show_markdown_table.py
+++ examples/show_markdown_table.py
@@ -1,10 +1,11 @@
 from resotoclient import ResotoClient
-
+import os
 
 # Create a ResotoClient object.
-# in case you have psk available, you can use it here so that
+# in case you configured psk, use it here so that
 # Resoto is able to verify the TLS certificate.
-client = ResotoClient(url="https://localhost:8900", psk=None)
+psk = os.environ.get("PSK", "changeme")
+client = ResotoClient(url="https://localhost:8900", psk=psk)
 
 # Search for all instances and return a markdown formatted table.
 for line in client.cli_execute("resoto", "search is(instance) | list --markdown"):
